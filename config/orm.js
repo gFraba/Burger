4db4c022e54d164956ea0d031ec534df0e8fcd76
gfraba <joshua.gagle@gmail.com>
2021-04-07 00:59:00 -0400

    adding orm and model

---
+++ config/orm.js
@@ -0,0 +1,95 @@
+const connection = require("../config/connection.js");
+
+// function for SQL syntax
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+};
+
+//convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+
+    for (let key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+};
+
+// object for SQL functions
+const orm = {
+    all: (tableInput, cb) => {
+        const queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    create: (table, cols, vals, cb) => {
+        const queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: (table, objColVals, condition, cb) => {
+        const queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    delete: (table, condition, cb) => {
+        let queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
