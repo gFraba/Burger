5df95fb889d94fe3710518c5c84ef17a37fb3b1b
gfraba <joshua.gagle@gmail.com>
2021-04-06 23:09:48 -0400

    fixed seeds syntax error, added connection config

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ USE burgers_db;
 INSERT INTO burger(burger_name, devoured) 
 VALUES
     ("Black and Blue", false),
-    ("Double Cheese Burger", false)
+    ("Double Cheese Burger", false),
     ("Rodeo Burger", false);
